64182d393df3415579d3deeb2b1713cf57683dfb
tengfei.li <tengfei.li@beebank.com>
2021-01-04 16:19:23 +0800

    设置 测试 test

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -41,60 +41,62 @@ Java_com_tengfei_jnitest_MainActivity_getAge(JNIEnv *env, jobject thiz, jint age
     return myage;
 }
 
+
+
 extern "C"
 JNIEXPORT jint JNICALL
-Java_com_tengfei_jnitest_HrbbdataUtils_data_1init(JNIEnv *env, jobject thiz, jchar app_url,
-                                                  jchar app_key, jchar app_secret, jchar app_enable,
-                                                  jchar public_app_name, jchar public_app_version) {
-//    return touch_init('http://130.1.10.158:8888/',"","","","",'1.0');
-    return 1;
-}extern "C"
-JNIEXPORT jint JNICALL
-Java_com_tengfei_jnitest_MainActivity_dataCreate(JNIEnv *env, jobject thiz) {
-    // TODO: implement dataCreate()
-    touch_create(3,100,1000,100,50,10);
-    return 1;
-}extern "C"
-JNIEXPORT jint JNICALL
-Java_com_tengfei_jnitest_MainActivity_data_1set_1print_1flag(JNIEnv *env, jobject thiz) {
+Java_com_tengfei_jnitest_MainActivity_touch_1set_1print_1flag(JNIEnv *env, jobject thiz) {
     // TODO: implement data_set_print_flag()
     touch_set_print_flag(1);
+    LOGE("touch_set_print_flag");
     return 1;
-}extern "C"
+}
+
+extern "C"
 JNIEXPORT jint JNICALL
-Java_com_tengfei_jnitest_MainActivity_data_1global_1create(JNIEnv *env, jobject thiz) {
+Java_com_tengfei_jnitest_MainActivity_touch_1global_1create(JNIEnv *env, jobject thiz) {
     // TODO: implement data_global_create()
     touch_global_create(3,100,1000,100,50,10);    /*线程池里最小1个线程，最大100个，队列最大值12,每包任务数，失败步长，任务轮询间隔*/
+    LOGE("touch_global_create");
     return 1;
-}extern "C"
+}
+
+extern "C"
 JNIEXPORT jint JNICALL
-Java_com_tengfei_jnitest_MainActivity_data_1global_1init(JNIEnv *env, jobject thiz) {
+Java_com_tengfei_jnitest_MainActivity_touch_1global_1init(JNIEnv *env, jobject thiz) {
     // TODO: implement data_global_init()
-   return touch_global_init( "http://130.1.10.158:8888/data", "default", "1qweasd!", "true", "app name", "1.0.0");
-}extern "C"
+   touch_global_init( "http://130.1.10.158:8888/data", "default", "1qweasd!", "true", "哈尔滨银行手机银行", "1.0.0");
+    LOGE("touch_global_init");
+    return 1;
+
+}
+
+extern "C"
 JNIEXPORT jint JNICALL
 Java_com_tengfei_jnitest_MainActivity_touch_1global_1registerSuperProperties(JNIEnv *env,
                                                                              jobject thiz) {
     // TODO: implement touch_global_registerSuperProperties()
-
+    touch_global_registerSuperProperties( "{\"card_no0\": 1234567890 }" );
+    touch_global_registerSuperProperties( "{\"card_no1\": \"11111111\", \"card_no2\": \"22222222\", \"card_no3\": \"33333333\"}" );
+    touch_global_registerSuperProperties( "{\"card_no9\": 1234567890123 }" );
+    touch_global_registerSuperProperties( "{\"card_no4\": \"44444444\",\"card_no5\": \"55555555\"}" );
     touch_global_registerSuperProperties( "{\"card_no6\": \"66666666\",\"card_no7\": \"77777777\", \"card_no5\": \"56565656\"}" );
+    LOGE("touch_global_registerSuperProperties");
     return 1;
-}extern "C"
+}
+
+extern "C"
 JNIEXPORT jint JNICALL
-Java_com_tengfei_jnitest_MainActivity_data_1track(JNIEnv *env, jobject thiz) {
+Java_com_tengfei_jnitest_MainActivity_touch_1track(JNIEnv *env, jobject thiz) {
     // TODO: implement data_track()
 
     char distinct_id[64+1]={0};
     char properties[128]={0};
-    int i=0;
 
-    for(i=1; i<=50; i++) {
-        sprintf(distinct_id, "%d", i);
-        sprintf(properties, "{\"no\": %d, \"card_no\": \"%06dXXXX\", \"bgint\":1234567890123}", i,i);
+        sprintf(properties, "{\"no\": %d, \"card_no\": \"%06dXXXX\", \"bgint\":1234567890123}");
         touch_global_track(distinct_id, 1, "AppClick", properties);
         LOGE("AppClick");
 
-    }
     return 1;
 
 }
--- app/src/main/java/com/tengfei/jnitest/MainActivity.java
+++ app/src/main/java/com/tengfei/jnitest/MainActivity.java
@@ -3,6 +3,7 @@ package com.tengfei.jnitest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,11 +23,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
         TextView tv_datainit=findViewById(R.id.tv_datainit);
+        TextView tv_touch_set_print_flag=findViewById(R.id.tv_touch_set_print_flag);
+        TextView tv_touch_global_create=findViewById(R.id.tv_touch_global_create);
+        TextView tv_touch_global_init=findViewById(R.id.tv_touch_global_init);
+        TextView tv_touch_global_registerSuperProperties=findViewById(R.id.tv_touch_global_registerSuperProperties);
+
         TextView tv_dataTrack=findViewById(R.id.tv_datatrack);
         tv.setText(stringFromJNI());
         tv.setText(stringFromJNI()+"   time:"+getTime(100)+"   age:"+getAge(30));
         tv.setOnClickListener(this);
         tv_datainit.setOnClickListener(this);
+        tv_touch_set_print_flag.setOnClickListener(this);
+        tv_touch_global_create.setOnClickListener(this);
+        tv_touch_global_init.setOnClickListener(this);
+        tv_touch_global_registerSuperProperties.setOnClickListener(this);
         tv_dataTrack.setOnClickListener(this);
 
     }
@@ -38,19 +48,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public native String stringFromJNI();
     public native int getTime(int time);
     public native int getAge(int age);
-    public native int data_set_print_flag();//设置是否打印日志
-    public native int dataCreate();//touchCreate()
+    public native int touch_set_print_flag();//设置是否打印日志
+//    public native int dataCreate();//touchCreate()
 
     //touch_global_create(3,100,1000,100,50,10);    /*线程池里最小1个线程，最大100个，队列最大值12,每包任务数，失败步长，任务轮询间隔*/
-    public native int data_global_create();
+    public native int touch_global_create();
 
     //touch_global_init( "http://130.1.10.158:8888/data", "default", "1qweasd!", "true", "app name", "1.0.0");
-    public native int data_global_init();
+    public native int touch_global_init();
 
     // touch_global_registerSuperProperties( "{\"card_no6\": \"66666666\",\"card_no7\": \"77777777\", \"card_no5\": \"56565656\"}" );
     public native int touch_global_registerSuperProperties();
 
-    public native int data_track();
+    public native int touch_track();
 
 
 
@@ -58,16 +68,31 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.tv_datainit://初始化
-                Toast.makeText(MainActivity.this,"dataInit",Toast.LENGTH_SHORT).show();
-                data_set_print_flag();
-                dataCreate();
-                data_global_create();
-                data_global_init();
+                touch_set_print_flag();
+                touch_global_create();
+                touch_global_init();
                 touch_global_registerSuperProperties();
                 break;
+            case R.id.tv_touch_set_print_flag:
+                touch_set_print_flag();
+                Log.e("Hrbbdata:", "touch_set_print_flag（）");
+                break;
+            case R.id.tv_touch_global_create:
+                touch_global_create();
+                Log.e("Hrbbdata:", "tv_touch_global_create（）");
+                break;
+            case R.id.tv_touch_global_init:
+                touch_global_init();
+                Log.e("Hrbbdata:", "touch_global_init（）");
+                break;
+            case R.id.tv_touch_global_registerSuperProperties:
+                touch_global_registerSuperProperties();
+                Log.e("Hrbbdata:", "touch_global_registerSuperProperties（）");
+                break;
             case R.id.tv_datatrack://datatrack
-                Toast.makeText(MainActivity.this,"dataTrack",Toast.LENGTH_SHORT).show();
-                data_track();
+//                Toast.makeText(MainActivity.this,"dataTrack",Toast.LENGTH_SHORT).show();
+                touch_track();
+                Log.e("Hrbbdata:", "data_track（）");
                     break;
             default:
                 break;
